a56fa2fef0e52f7286296452f4b9db7af81533b4
jereme-capulong <jereme.capulong@outlook.com>
2026-02-16 00:20:01 +0800

    Update envDir

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "renderer",
+  envDir: "..",
   // Required for Electron file:// loading in production builds.
   base: "./",
   plugins: [react()],
